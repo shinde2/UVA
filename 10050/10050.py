8d629a6a343527830e14ce7b4df0a1cd9b13bb73
Ashish H Shinde <ahs278@cornell.edu>
2021-03-23 23:24:51 -0700

    problems

---
+++ 10050/10050.py
@@ -0,0 +1,38 @@
+from math import ceil
+
+
+def hartal(params):
+
+    lost = 0
+    days = params[0]
+
+    weeks = [[0]*6 for _ in range(int(ceil(days/7))+1)]
+
+    for party in params[1:]:
+        i = party
+        while i <= days:
+            d = i % 7
+            w = int(ceil(i/7))
+            if d != 6 and d != 7:
+                weeks[w][d] = 1
+
+            i += i
+
+    for week in weeks[1:]:
+        for day in week[1:]:
+            if day == 1:
+                lost += 1
+
+    print(weeks)
+    print(lost)
+
+
+def main():
+
+    params = [14, 4]
+
+    hartal(params)
+
+
+if __name__ == '__main__':
+    main()
