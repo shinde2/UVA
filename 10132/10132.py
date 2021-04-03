87077f0da5743d8cf19302d1344628a0314ef16d
Ashish H Shinde <ahs278@cornell.edu>
2021-04-02 20:09:22 -0700

    probs

--- 10132/10132.py
+++ 10132/10132.py
@@ -1,7 +1,11 @@
+# tests passed
 
-def solution_found(output, n):
+from copy import deepcopy
 
-    if len(output) == n:
+
+def solution_found(output, k, n):
+
+    if k == n-1:
         i = 0
         while i < n-2:
             if output[i]+output[i+1] == output[i+2]+output[i+3]:
@@ -15,13 +19,18 @@ def solution_found(output, n):
 
 def get_possibilities(files, output, k):
 
-    return [file for file in files if file not in output[0:k]]
+    posiible_files = deepcopy(files)
+
+    for file in output[0:k]:
+        posiible_files.remove(file)
+
+    return posiible_files
 
 
 def check_files(files, output, k, n):
 
-    if solution_found(output, n):
-        print(output)
+    if solution_found(output, k, n):
+        return output
     else:
         k += 1
         possibilities = get_possibilities(files, output, k)
@@ -30,24 +39,36 @@ def check_files(files, output, k, n):
                 output[k] = possibility
             except:
                 output.append(possibility)
-            check_files(files, output, k, n)
+            solun = check_files(files, output, k, n)
+            if solun:
+                return solun
 
 
 def main():
 
-    files = [
-        "011",
-        #"0111",
-        "01110",
-        "111",
-        #"0111",
-        "10111"
-    ]
+    #files = [
+    #    "011",
+    #    "0111",
+    #    "01110",
+    #    "111",
+    #    "0111",
+    #    "10111"
+    #]
 
-    output = list()
     k = -1
+    files = list()
 
-    check_files(files, output, k, len(files))
+    with open("tests.txt") as f:
+        lines = f.readlines()
+    lines.append("\n")
+
+    for line in lines[2:]:
+        if not line.strip():
+            solutn = check_files(files, [], k, len(files))
+            print("".join(solutn[0:2]))
+            files = []
+        else:
+            files.append(line.rstrip())
 
 
 if __name__ == '__main__':
