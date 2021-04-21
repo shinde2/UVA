0457683e3cd480ff137884880f0b9b85533773e9
Ashish H Shinde <ahs278@cornell.edu>
2021-04-20 23:26:26 -0700

    num fibs

---
+++ 10183/10183.py
@@ -0,0 +1,43 @@
+
+
+def NumFibs(a, b):
+
+    if a == 0 and b == 0:
+        return
+
+    if a == b or a+1 == b:
+        print("0")
+        return
+
+    if a == 0:
+        prev = 0
+        a = 1
+    else:
+        if a % 2 == 0:
+            prev = a/2 - 1
+        else:
+            prev = (a+1)/2 - 1
+
+    count = 0
+    curr = prev + a
+
+    while curr <= b:
+        count += 1
+        prev = a
+        a = curr
+        curr = prev + a
+
+    print(count)
+
+
+def main():
+
+    with open("inputs.txt") as infile:
+        lines = infile.readlines()
+
+    for line in lines:
+        NumFibs(*map(int, line.strip().split()))
+
+
+if __name__ == '__main__':
+    main()
