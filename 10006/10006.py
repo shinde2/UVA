351345781e565f4f5500cb1a265f45fa2c822db4
Ashish H Shinde <ahs278@cornell.edu>
2021-05-03 10:56:29 -0700

    charmichael

--- 10006/10006.py
+++ 10006/10006.py
@@ -1,6 +1,20 @@
 # a^n mod n
 
 
+def main():
+
+    a = 4
+    n = 12
+    N = 12
+
+    res = a
+    while N-1:
+        res = res * a
+        res = res % n
+        N -= 1
+
+    print(res)
+
 
 if __name__ == '__main__':
     main()
