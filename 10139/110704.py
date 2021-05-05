844130314c79cdb72963516378b2d07413cc1500
Ashish H Shinde <ahs278@cornell.edu>
2021-05-05 09:22:03 -0700

    factovisors

---
+++ 10139/110704.py
@@ -0,0 +1,29 @@
+# first solution is too slow for large integers
+
+
+def first():
+
+    with open("inputs.txt") as i:
+        lines = i.readlines()
+
+    for line in lines:
+        n, m = map(int, line.strip().split())
+
+        f = n
+
+        res = 1
+        while f:
+            if res == 0:
+                break
+            res *= f
+            res %= m
+            f -= 1
+
+        if res == 0:
+            print(f"{m} divides {n}!")
+        else:
+            print(f"{m} does not divide {n}!")
+
+
+if __name__ == '__main__':
+    first()
