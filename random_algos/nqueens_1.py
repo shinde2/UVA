8fc1b567cb48ffb58d9970f309cd21c22c3f539b
Ashish H Shinde <ahs278@cornell.edu>
2021-05-21 23:20:23 -0700

    nqueens 1

--- random_algos/nqueens_1.py
+++ random_algos/nqueens_1.py
@@ -1,44 +1,76 @@
 from pprint import pprint as print
-from copy import deepcopy
 
 
 num_ways = 0
 
 
-def found(N, queen):
+def found(queen, grid):
 
-    return queen == N
+    return queen == len(grid)
 
 
-def possible_positions(N, grid):
+def possible_positions(queen, grid):
 
     possibilities = list()
+    N = len(grid)
+    high = N * N
+    low = 0 if queen == 0 else (int(grid[queen-1] / N) + 1) * N
+
+    for n in range(low, high):
+        for q in range(0, queen):
+            if q % N == n % N:
+                break
+        else:
+            possibilities.append(n)
+
+    for q in range(0, queen):
+        n = grid[q]
+        while n < N * N:
+            if n in possibilities:
+                possibilities.remove(n)
+            n += 3
+        n = grid[q]
+        while n < N * N:
+            if n in possibilities:
+                possibilities.remove(n)
+            n += 5
+        n = grid[q]
+        while n >= 0:
+            if n in possibilities:
+                possibilities.remove(n)
+            n -= 5
+        n = grid[q]
+        while n >= 0:
+            if n in possibilities:
+                possibilities.remove(n)
+            n -= 3
 
     return possibilities
 
 
-def nqueens(N, queen, grid, solutions):
+def nqueens(queen, grid):
 
     global num_ways
-    if found(N, queen):
+    if found(queen, grid):
         num_ways += 1
     else:
-        possibilities = possible_positions(N, grid)
-        for x, y in possibilities:
-            grid[x][y] = 1
-            nqueens(N, queen+1, grid, solutions)
-            grid[x][y] = 0
+        possibilities = possible_positions(queen, grid)
+        #print(queen)
+        #print(possibilities)
+        for possibility in possibilities:
+            grid[queen] = possibility
+            nqueens(queen+1, grid)
 
 
 def main():
 
-    N = 8
+    N = 4
     queen = 0
     grid = [-1 for _ in range(N)]
     global num_ways
 
     num_ways = 0
-    nqueens(N, queen, grid, [])
+    nqueens(queen, grid)
     print(num_ways)
 
 
