ba3ea577292388b31852476e70faae554d85ca08
Ashish H Shinde <ahs278@cornell.edu>
2021-10-25 01:53:03 -0700

    SegmentTreeBasic

---
+++ random_algos/SegmentTreeBasic.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+#define init() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define debug1(x) cout << x << endl;
+#define debug2(x, y) cout << x << " " << y << endl;
+#define ll long long
+int mx = 1e2;
+vector<int> st(4 * mx);
+
+
+int sum(int v, int l, int r, int li, int ri){
+
+    if(li == l and ri == r) return st[v];
+    else{
+        int mid = (l + r) / 2;
+        if(ri <= mid) return sum(2 * v, l, mid, li, ri);
+        else if(li > mid) return sum((2 * v) + 1, mid + 1, r, li, ri);
+        else return (sum(2 * v, l, mid, li, mid) +
+                     sum((2 * v) + 1, mid + 1, r, mid + 1, ri));
+    }
+
+}
+
+
+void build(vector<int>& arr, int v, int l, int r){
+
+    if(l == r) st[v] = arr[l];
+    else{
+        int mid = (l + r) / 2;
+        build(arr, 2 * v, l, mid);
+        build(arr, (2 * v) + 1, mid + 1, r);
+        st[v] = st[2 * v] + st[(2 * v) + 1];
+    }
+
+}
+
+
+void solve(){
+
+    int n, li, ri;
+    cin >> n >> li >> ri;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    build(arr, 1, 0, n - 1);
+    int sum_q = sum(1, 0, n - 1, li, ri);
+    cout << sum_q << "\n";
+}
+
+
+int main() {
+
+	init();
+
+	//int t;
+	//cin >> t;
+	//while(t--)
+	    solve();
+
+	return 0;
+}
+
+
+
