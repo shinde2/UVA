99a19adc114fc816b6f9ed052f6937818119bced
Ashish H Shinde <ahs278@cornell.edu>
2021-10-25 02:08:45 -0700

    SegmentTreeBasic

--- random_algos/SegmentTreeBasic.cpp
+++ random_algos/SegmentTreeBasic.cpp
@@ -9,6 +9,19 @@ int mx = 1e2;
 vector<int> st(4 * mx);
 
 
+int update(int v, int l, int r, int ui, int uv){
+
+    if(l == r) st[v] = uv;
+    else{
+        int mid = (l + r) / 2;
+        if(ui <= mid) update(2 * v, l, mid, ui, uv);
+        else update((2 * v) + 1, mid + 1, r, ui, uv);
+        st[v] = st[2 * v] + st[(2 * v) + 1];
+    }
+
+}
+
+
 int sum(int v, int l, int r, int li, int ri){
 
     if(li > ri) return 0;
@@ -37,14 +50,21 @@ void build(vector<int>& arr, int v, int l, int r){
 
 void solve(){
 
-    int n, li, ri;
-    cin >> n >> li >> ri;
+    int n, li, ri, ui, uv, sum_q;
+    cin >> n >> li >> ri >> ui >> uv;
     vector<int> arr(n);
     for(int i = 0; i < n; i++) cin >> arr[i];
 
     build(arr, 1, 0, n - 1);
-    int sum_q = sum(1, 0, n - 1, li, ri);
+
+    sum_q = sum(1, 0, n - 1, li, ri);
     cout << sum_q << "\n";
+
+    update(1, 0, n - 1, 2, 3);
+
+    sum_q = sum(1, 0, n - 1, li, ri);
+    cout << sum_q << "\n";
+
 }
 
 
